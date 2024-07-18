4dc07f26c77dd0b170ff193f11a57f69b67d6a3a
JeremySpence272 <jeremy.spence272@gmail.com>
2024-07-18 18:31:41 -0400

    update margins

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,16 @@ const App: React.FC = () => {
       <h1 className="text-xl mt-16 ml-12 font-bold">coming soon... </h1>
       <p className="ml-12 italic"> in the meantime:</p>
       <p className="ml-12 mt-4">
-        linkedin:
         <a
-          className="ml-4 text-slate-500 underline"
+          className="text-slate-500 underline"
           href="https://www.linkedin.com/in/jspence272/"
         >
-          link
+          linkedin
         </a>
       </p>
       <p className="ml-12">
-        email:
         <a
-          className="ml-4 text-slate-500 underline"
+          className="text-slate-500 underline"
           href="mailto@j.spence@columbia.edu"
         >
           j.spence@columbia.edu
